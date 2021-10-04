0f2d25a480eecede5f8111ea7ba1c73f736f4476
param108 <param.ponnaiyan@gmail.com>
2021-10-04 23:24:53 +0530

    [Param] Added a test for eventbus

---
+++ eventbus/eventbus_test.go
@@ -0,0 +1,68 @@
+package eventbus
+
+import (
+	"testing"
+	"time"
+
+	"github.com/param108/datatable/messages"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventBus(t *testing.T) {
+	eb := NewEventBus()
+
+	clt1rd, clt1wr := eb.RegisterWindow()
+
+	clt2rd, clt2wr := eb.RegisterWindow()
+
+	t.Run("write from clt2 is available to all others", func(t *testing.T) {
+		msg := &messages.Message{
+			Key: "test_message",
+		}
+
+		timeout := time.NewTimer(time.Second * 10)
+		clt2wr <- msg
+		select {
+		case recvd := <-clt1rd:
+			timeout.Stop()
+			assert.Equal(t, "test_message", string(recvd.Key))
+		case <-timeout.C:
+			assert.False(t, true, "Timeout when waiting for message")
+		}
+
+		select {
+		case recvd := <-clt2rd:
+			timeout.Stop()
+			assert.Equal(t, "test_message", string(recvd.Key))
+		case <-timeout.C:
+			assert.False(t, true, "Timeout when waiting for message")
+		}
+
+	})
+
+	t.Run("write from clt2 is available to all others", func(t *testing.T) {
+		msg := &messages.Message{
+			Key: "test_message",
+		}
+
+		timeout := time.NewTimer(time.Second * 10)
+		clt1wr <- msg
+		select {
+		case recvd := <-clt1rd:
+			timeout.Stop()
+			assert.Equal(t, "test_message", string(recvd.Key))
+		case <-timeout.C:
+			assert.False(t, true, "Timeout when waiting for message")
+		}
+
+		select {
+		case recvd := <-clt2rd:
+			timeout.Stop()
+			assert.Equal(t, "test_message", string(recvd.Key))
+		case <-timeout.C:
+			assert.False(t, true, "Timeout when waiting for message")
+		}
+
+	})
+
+}
